fc57b67063a6e037435880b81eb4d68ccf8130a6
Austin Gunsauley <ajgunsauley@gmail.com>
2019-08-03 12:25:28 -0700

    added win condition for hero touching endzone

--- OneTeleport/Assets/Scripts/EndStateController.cs
+++ OneTeleport/Assets/Scripts/EndStateController.cs
@@ -1,19 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndStateController : MonoBehaviour
 {
+    public Text endgameText;
+    public bool levelComplete;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        levelComplete = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //We will need to edit how this works depending on how many levels we have.  Probably an input array
+        if (Input.GetMouseButton(0) & levelComplete == true)
+        {
+            Application.LoadLevel("EndLevel");
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -22,7 +30,17 @@ public class EndStateController : MonoBehaviour
         {
 
             Destroy(other.gameObject);
+            ShowMessage("You win!  Press Enter to continue...");
 
         }
+        
     }
+
+    void ShowMessage(string message)
+    {
+        endgameText.text = message;
+        endgameText.enabled = true;
+        levelComplete = true;
+    }
+
 }
