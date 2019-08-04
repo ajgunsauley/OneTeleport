43b7ccdbd4cfb0ebb8e2a434e58fcb2620156a8e
Austin Gunsauley <ajgunsauley@gmail.com>
2019-08-03 17:53:50 -0700

    drone acts as moving platform for player

---
+++ OneTeleport/Assets/Scripts/HeroController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroController : MonoBehaviour
+{
+    private Transform m_currDrone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "DroneRoof")
+        {
+            m_currDrone = coll.gameObject.transform;
+            transform.SetParent(m_currDrone);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "DroneRoof")
+        {
+            m_currDrone = null;
+            transform.parent = null;
+        }
+
+    }
+
+}
