da298913a8c6050ca1344460368564cd10cb0d0e
Thierry Treyer <ttreyer@fb.com>
2019-08-17 16:15:05 -0700

    Fix Chunky's death SFX

--- OneTeleport/Assets/Scripts/HeroController.cs
+++ OneTeleport/Assets/Scripts/HeroController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class HeroController : MonoBehaviour {
     private Animator animator;
 
+    // Group death SFX in the same object, detach it on deatch
+    public GameObject deathSFX;
     public AudioSource glubSound, frySound;
 
     // Start is called before the first frame update
@@ -13,14 +15,27 @@ public class HeroController : MonoBehaviour {
     }
 
     public void Die(bool playFrying = false) {
-        GetComponentInChildren<SpriteRenderer>().enabled = false;
-        GetComponentInChildren<LineRenderer>().enabled = false;
+        // Detach SFX so we can destroy the Hero imediatly, but keep the sound playing
+        deathSFX.transform.parent = null;
+
+        // Play the death sound
+        glubSound.enabled = true;
+        frySound.enabled = true;
+
         glubSound.Play();
         if (playFrying) frySound.Play();
+
+        // Destroy both object at different point in time
+        Destroy(gameObject);
+        Destroy(deathSFX, 1f);
     }
 
     public void Win() {
         animator.SetTrigger("DoWin");
+        GetComponentInChildren<LineRenderer>().enabled = false;
+        GetComponentInChildren<ParticleSystem>().Stop();
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
         Destroy(gameObject, 2f);
     }
 }
