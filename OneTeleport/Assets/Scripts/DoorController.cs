6e73c4a620cb556b58505777c7f5223c391707e8
Thierry Treyer <ttreyer@fb.com>
2019-08-18 15:36:22 -0700

    Animate door fade in/out

---
+++ OneTeleport/Assets/Scripts/DoorController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+    private Collider2D doorCollider;
+    private Animator animator;
+
+    private void Start() {
+        doorCollider = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void SetFieldActive(bool state) {
+        doorCollider.enabled = state;
+        animator.SetBool("IsEnabled", state);
+    }
+}
