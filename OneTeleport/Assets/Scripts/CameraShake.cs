a49a74d14ef0a2d637a1783df2bd86bdebd0109f
Thierry Treyer <ttreyer@fb.com>
2019-08-18 01:55:19 -0700

    Add camera shaking on chunky landing

---
+++ OneTeleport/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+    public IEnumerator Shake(float shake, Vector2 magnitude) {
+        Vector3 originalPos = transform.localPosition;
+
+        float shakeEnd = Time.time + shake;
+        while (Time.time < shakeEnd) {
+            float x = Random.Range(-1f, 1f) * magnitude.x;
+            float y = Random.Range(-1f, 1f) * magnitude.y;
+
+            transform.localPosition = new Vector3(x, y, originalPos.z);
+
+            yield return null;
+        }
+
+        transform.localPosition = originalPos;
+    }
+}
--- OneTeleport/Assets/Scripts/HeroController.cs
+++ OneTeleport/Assets/Scripts/HeroController.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class HeroController : MonoBehaviour {
     private Animator animator;
+    private CameraShake cameraShake;
+    private bool wasGrounded = true;
+    private Rigidbody2D rbody;
 
     // Group death SFX in the same object, detach it on deatch
     public GameObject deathSFX;
     public AudioSource glubSound, frySound;
+    public LayerMask groundLayer;
+    public Vector2 landDetectOffset, landShake;
+
+    public Vector3 landSettings;
 
     // Start is called before the first frame update
     void Start() {
         animator = GetComponent<Animator>();
+        cameraShake = Camera.main.GetComponent<CameraShake>();
+        rbody = GetComponent<Rigidbody2D>();
     }
 
     public void Die(bool playFrying = false) {
@@ -37,5 +46,24 @@ public class HeroController : MonoBehaviour {
         GetComponent<Collider2D>().enabled = false;
         GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
         Destroy(gameObject, 2f);
+    }
+
+    private void Land() {
+        float magnitude = rbody.velocity.magnitude;
+        magnitude = Mathf.Pow(magnitude, landSettings.z);
+        Debug.Log("LANDING " + magnitude);
+        magnitude = Mathf.Clamp(magnitude, landSettings.x, landSettings.y);
+
+        StartCoroutine(cameraShake.Shake(.15f, magnitude * landShake));
+    }
+
+    private void FixedUpdate() {
+        //Debug.Log(Physics2D.OverlapCircle((Vector2)transform.position + landDetectOffset, .5f, groundLayer));
+        bool isGrounded = Physics2D.OverlapCircle((Vector2)transform.position + landDetectOffset, .2f, groundLayer);
+        //Debug.Log(wasGrounded + " => " + isGrounded);
+        if (!wasGrounded && isGrounded)
+            Land();
+
+        wasGrounded = isGrounded;
     }
 }
