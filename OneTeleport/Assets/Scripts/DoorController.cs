6b046e783629294bf8552ea3dc79f21992ad5905
Chris M <vassarguy@aol.com>
2020-01-11 18:51:07 -0800

    2 new levels

--- OneTeleport/Assets/Scripts/DoorController.cs
+++ OneTeleport/Assets/Scripts/DoorController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorController : MonoBehaviour {
+    public bool state_ = true;
+
     private Collider2D doorCollider;
     private Animator animator;
 
     private void Start() {
         doorCollider = GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
+        SetFieldActive(state_);
     }
 
     public void SetFieldActive(bool state) {
+        state_ = state;
         doorCollider.enabled = state;
         animator.SetBool("IsEnabled", state);
     }
